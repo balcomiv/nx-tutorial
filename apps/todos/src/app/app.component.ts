9e41da04feebfa2a1ac11df5d178af1abdc3147c
Lyle Balcom <balcomiv@gmail.com>
2020-03-11 04:25:03 -0400

    Updating e2e tests for todos

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+}
+
 @Component({
   selector: 'nx-tutorial-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [];
 }
