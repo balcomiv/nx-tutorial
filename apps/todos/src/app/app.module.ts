68f996a6d9f52edb1078b1f416994cc91075a371
balcomiv <35431931+balcomiv@users.noreply.github.com>
2020-03-13 05:41:49 -0400

    Add - Section 4 (Connect to API)

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   providers: [],
