5aa6c27be85d606cd1c7732c544ea86530ba3580
balcomiv <35431931+balcomiv@users.noreply.github.com>
2020-03-14 19:29:47 -0400

    Updates/Corrections - todos-e2e

--- apps/todos-e2e/src/integration/app.spec.ts
+++ apps/todos-e2e/src/integration/app.spec.ts
@@ -8,7 +8,7 @@ describe('todos', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Todos');
+    getGreeting().contains('Welcome');
   });
 
   it('should display todos', () => {
